3d27b0e9f5b093119f9db06d1486eeeb5d474c50
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-06 11:31:28 +0300

    Updating the Location

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+const cityForm = document.querySelector('form');
+
+const updateCity = async (city) => {
+	const cityDets = await getCity(city);
+	const weather = await getWeather(cityDets);
+
+	return {
+		cityDets: cityDets,
+		weather: weather,
+	};
+};
+
+cityForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	// get city value
+	const city = cityForm.city.value.trim();
+	cityForm.reset();
+
+	// update ui w/ new city
+	updateCity(city)
+		.then((data) => {
+			console.log(data);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
