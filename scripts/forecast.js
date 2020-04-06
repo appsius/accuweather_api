cc6da75a6d5e5770529adfc6e073436db25c518a
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-06 11:15:43 +0300

    Ger Weather Api Call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,17 @@
-const key = 'Mkd6K2tE0bYW65zlYwsC45XTG1y8FcBA';
+const key = '';
 
+// Get weather information
+const getWeather = async (id) => {
+	const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+	const query = `${id}?apikey=${key}`;
+
+	const response = await fetch(base + query);
+	const data = await response.json();
+
+	return data[0];
+};
+
+// Get city information
 const getCity = async (city) => {
 	const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
 	const query = `?apikey=${key}&q=${city}`;
@@ -11,5 +23,10 @@ const getCity = async (city) => {
 };
 
 getCity('ankara')
-	.then((data) => console.log(data))
+	.then((data) => {
+		return getWeather(data.Key);
+	})
+	.then((data) => {
+		console.log(data);
+	})
 	.catch((err) => console.log(err));
