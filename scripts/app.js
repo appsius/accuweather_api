028c59ca30421fa45d2918c4c0357ccf11e62d91
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-07 21:03:41 +0300

    Making Forecast Class

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
 
 const updateUI = (data) => {
 	// destructure properties
@@ -30,13 +31,6 @@ const updateUI = (data) => {
 	}
 };
 
-const updateCity = async (city) => {
-	const cityDets = await getCity(city);
-	const weather = await getWeather(cityDets.Key);
-
-	return { cityDets, weather };
-};
-
 cityForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 
@@ -45,7 +39,8 @@ cityForm.addEventListener('submit', (e) => {
 	cityForm.reset();
 
 	// update ui w/ new city
-	updateCity(city)
+	forecast
+		.updateCity(city)
 		.then((data) => updateUI(data))
 		.catch((err) => console.log(err));
 
@@ -54,7 +49,8 @@ cityForm.addEventListener('submit', (e) => {
 });
 
 if (localStorage.getItem('city')) {
-	updateCity(localStorage.getItem('city'))
+	forecast
+		.updateCity(localStorage.getItem('city'))
 		.then((data) => updateUI(data))
 		.catch((err) => console.log(err));
 }
